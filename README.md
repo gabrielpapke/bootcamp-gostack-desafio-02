09889fd448a7d19226dca7b802957b5adf760bdb
Gabriel Papke <gabrielpapke@gmail.com>
2020-06-27 14:34:12 -0300

    Removendo repositório e aplicando middlewares

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ Será uma aplicação para armazenar repositórios de um portfólio, que irá pe
 
 - [x] **`PUT /repositories/:id`**: A rota deve alterar apenas o `title`, a `url` e as `techs` do repositório que possua o `id` igual ao `id` presente nos parâmetros da rota;
 
-- [ ] **`DELETE /repositories/:id`**: A rota deve deletar o repositório com o `id` presente nos parâmetros da rota;
+- [x] **`DELETE /repositories/:id`**: A rota deve deletar o repositório com o `id` presente nos parâmetros da rota;
 
 - [ ] **`POST /repositories/:id/like`**: A rota deve aumentar o número de likes do repositório específico escolhido através do `id` presente nos parâmetros da rota, a cada chamada dessa rota, o número de likes deve ser aumentado em 1;
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,6 +9,33 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function validateRepositoryId (request, response, next) {
+  const { id } = request.params
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid repository ID"})
+  }
+
+  next()
+
+}
+
+function validateRepositoryFinded (request, response, next) {
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found "})
+  }
+
+  response.locals.repositoryIndex = repositoryIndex;
+
+  next()
+
+}
+
+app.use('/repositories/:id', validateRepositoryId, validateRepositoryFinded)
 
 app.get("/repositories", (request, response) => response.json(repositories));
 
@@ -29,14 +56,8 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params
   const { title, url, techs } = request.body
-
-  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
-
-  if (repositoryIndex < 0) {
-    return response.status(400).json({ error: "Repository not found "})
-  }
+  const { repositoryIndex } = response.locals;
 
   const updatedRepository = {
     ...repositories[repositoryIndex],
@@ -51,7 +72,11 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { repositoryIndex } = response.locals;
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
